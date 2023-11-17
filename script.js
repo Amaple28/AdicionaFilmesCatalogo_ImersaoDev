62be00691fd7b75ffc3b82219eeb47fd894cdfa9
Maisa Rodrigues <maisagabirodrigues@gmail.com>
2023-11-17 15:25:32 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function adicionarFilme(){
+    var filmeFavorito = document.getElementById("filme").value;
+    var elementoListaFilmes = document.getElementById("listaFilmes");
+
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + 
+        "<img src=" + filmeFavorito + ">";
+
+    document.getElementById("filme").value = "";
+
+}
